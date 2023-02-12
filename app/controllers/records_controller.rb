d7d67e18bdd62293b093a830b6fa67359ff08374
Edwin Portillo <edwin.portillo@calltrackingmetrics.com>
2023-02-11 21:40:16 -0500

    some basic crud stuff

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,24 @@
 class RecordsController < ApplicationController
   def index
+    @records = Record.all
+  end
+
+  def new
+  end
+
+  def create
+    @record = Record.new(record_params)
+
+    @record.save
+    redirect_to @record
+  end
+
+  def show
+    @record = Record.find(params[:id])
+  end
+
+  private
+  def record_params
+    params.require(:record).permit(:username)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "records#index"
+
+  resources :records
   get "/records", to: "records#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
