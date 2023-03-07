8a2d4656ff51c958d758b3ef02dbf76cd6b9d6fc
Edwin Portillo <edwin.portillo@calltrackingmetrics.com>
2023-03-07 17:52:17 -0500

    api request to get the 200 most recent scrobbles, need to refactor shit

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,8 @@
 class RecordsController < ApplicationController
+  require 'net/http'
+
+  LAST_FM_API_KEY = "11f12d6b2aae4b2b41e2abc116d687fd"
+
   def index
     @records = Record.all
   end
@@ -9,11 +13,17 @@ class RecordsController < ApplicationController
   def create
     @record = Record.new(record_params.merge(:start_date => Date.today - (params[:record][:months]).to_i.months))
     @record.save!
-    redirect_to @record
+
+
+    res = JSON.parse(Net::HTTP.get_response(URI("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=#{@record.username}&limit=200&from=#{(@record.created_at - @record.months.months).utc}&to=#{@record.created_at.utc}&api_key=#{LAST_FM_API_KEY}&format=json")).body)
+    songs = res["recenttracks"]["track"].map{|track| track["name"]}
+
+    redirect_to record_path(@record, songs: songs)
   end
 
   def show
     @record = Record.find(params[:id])
+    @songs = params[:songs]
   end
 
   private
