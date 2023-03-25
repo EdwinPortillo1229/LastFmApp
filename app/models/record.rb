21e9b5a23648d50f921f1a2feb9bc5590a077316
Edwin Portillo <edwin.portillo@calltrackingmetrics.com>
2023-03-25 14:31:50 -0400

    meta-dataing it

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,7 +1,17 @@
 class Record < ApplicationRecord
   def get_all_songs(first_page)
+    songs = {}
     total_pages = first_page["recenttracks"]["@attr"]["totalPages"].to_i
-    songs = first_page["recenttracks"]["track"].map{|track| track["name"]}
+    songs = first_page["recenttracks"]["track"].map do |track|
+      {
+        "unaltered_name" => track["name"],
+        "unaltered_artist" => track["artist"]["#text"],
+        "unalter_album" => track["album"]["#text"],
+        "name" =>  track["name"].gsub(/\s+/, "").gsub(/[^0-9a-z ]/i, '').downcase,
+        "artist" => track["artist"]["#text"].gsub(/\s+/, "").gsub(/[^0-9a-z ]/i, '').downcase,
+        "album" => track["album"]["#text"].gsub(/\s+/, "").gsub(/[^0-9a-z ]/i, '').downcase,
+      }
+    end
     songs
   end
 end
