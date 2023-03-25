e33873813c43b45429b7290de8ed68607f6999b8
Edwin Portillo <edwin.portillo@calltrackingmetrics.com>
2023-03-25 14:22:28 -0400

    moving logic to model

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -11,13 +11,13 @@ class RecordsController < ApplicationController
   end
 
   def create
-    debugger
     @record = Record.new(record_params.merge(:start_date => Date.today - (params[:record][:months]).to_i.months))
     @record.save!
 
 
-    res = JSON.parse(Net::HTTP.get_response(URI("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=#{@record.username}&limit=200&from=#{(@record.created_at - @record.months.months).utc}&to=#{@record.created_at.utc}&api_key=#{LAST_FM_API_KEY}&format=json")).body)
-    songs = res["recenttracks"]["track"].map{|track| track["name"]}
+    first_page = JSON.parse(Net::HTTP.get_response(URI("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=#{@record.username}&page=1&limit=200&from=#{(@record.created_at - @record.months.months).utc}&to=#{@record.created_at.utc}&api_key=#{LAST_FM_API_KEY}&format=json")).body)
+    songs = @record.get_all_songs(first_page)
+    debugger
 
     redirect_to record_path(@record, songs: songs)
   end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,7 @@
 class Record < ApplicationRecord
+  def get_all_songs(first_page)
+    total_pages = first_page["recenttracks"]["@attr"]["totalPages"].to_i
+    songs = first_page["recenttracks"]["track"].map{|track| track["name"]}
+    songs
+  end
 end
