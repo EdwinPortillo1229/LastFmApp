03c3152bed9d0a9c92f01d323b1c031a65d8684c
Edwin Portillo <edwinportillo@Edwins-MacBook-Pro.local>
2023-07-21 22:30:47 -0400

    Revert "ok"
    This reverts commit ea20952fa08b07bb23613ce4f147892723789a26.

--- db/migrate/20230210232912_create_records.rb
+++ db/migrate/20230210232912_create_records.rb
@@ -1,4 +1,4 @@
-class CreateRecords < ActiveRecord::Migration[6.1]
+class CreateRecords < ActiveRecord::Migration[7.0]
   def change
     create_table :records do |t|
       t.string :username
--- db/migrate/20230325185723_change_months_to_string_in_records.rb
+++ db/migrate/20230325185723_change_months_to_string_in_records.rb
@@ -1,4 +1,4 @@
-class ChangeMonthsToStringInRecords < ActiveRecord::Migration[6.1]
+class ChangeMonthsToStringInRecords < ActiveRecord::Migration[7.0]
   def change
     change_column :records, :months, :string
   end
